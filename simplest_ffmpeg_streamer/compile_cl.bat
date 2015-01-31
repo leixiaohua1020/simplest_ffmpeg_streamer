5d5ca50b8cb0d36badb937e6618c58bb6a47dba9
unknown <leixiaohua1020@126.com>
2015-01-31 18:52:47 +0800

    compile cmd

---
+++ simplest_ffmpeg_streamer/compile_cl.bat
@@ -0,0 +1,16 @@
+::最简单的基于FFmpeg的推流器（推送RTMP）----命令行编译
+::Simplest FFmpeg Streamer (Send RTMP)----Compile in Cmd 
+::
+::雷霄骅 Lei Xiaohua
+::leixiaohua1020@126.com
+::中国传媒大学/数字电视技术
+::Communication University of China / Digital TV Technology
+::http://blog.csdn.net/leixiaohua1020
+::
+::include
+@set INCLUDE=include;%INCLUDE%;
+::lib
+@set LIB=lib;%LIB%;
+::compile and link
+cl simplest_ffmpeg_streamer.cpp /link avcodec.lib avformat.lib avutil.lib ^
+avdevice.lib avfilter.lib postproc.lib swresample.lib swscale.lib /OPT:NOREF
